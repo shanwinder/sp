5cb727d90f58ef2b263c1a6cdd7abf7dec2b9ba0
shanwinder <poobpub153@gmail.com>
2025-06-15 14:21:06 +0700

    Update db.php

--- includes/db.php
+++ includes/db.php
@@ -9,4 +9,7 @@ $conn = new mysqli($host, $user, $pass, $db);
 if ($conn->connect_error) {
     die("เชื่อมต่อฐานข้อมูลล้มเหลว: " . $conn->connect_error);
 }
+
+$conn->set_charset("utf8mb4"); // หรือ utf8mb4
+
 ?>
